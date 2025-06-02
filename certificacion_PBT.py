ff4f6f26cb6ea7385d43699174ebf264616e9a7f
Hernan Miranda <210466929+ciberuniverse@users.noreply.github.com>
2025-06-02 01:05:12 -0400

    PBT Certificacion

---
+++ certificacion_PBT.py
@@ -0,0 +1,14 @@
+import certi
+
+certi.FILE_CODE = "certificacion_PBT"
+
+lista_acendente = []
+cantidad_de_vocales = 0
+
+############################# TU CODIGO AQUI ABAJO
+certi.ID = "TU_ID_AQUI"
+
+
+
+############################# Estas son las variables que se enviaran como respuesta
+certi.resolve_tecnical(lista_acendente, cantidad_de_vocales)
